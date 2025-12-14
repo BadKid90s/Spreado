d0b2a65a0807e6f8896afde541ccb7c8719b1008
wangruiyu <wry10150@163.com>
2025-12-14 18:32:11 +0800

    feat(main): 修改cookie路径问题

--- examples/get_douyin_cookie.py
+++ examples/get_douyin_cookie.py
@@ -5,6 +5,6 @@ from conf import BASE_DIR
 from uploader.douyin_uploader.main import douyin_setup
 
 if __name__ == '__main__':
-    account_file = Path(BASE_DIR / "examples" / "cookies" / "douyin_uploader" / "account.json")
+    account_file = Path(BASE_DIR  / "cookies" / "douyin_uploader" / "account.json")
     account_file.parent.mkdir(parents=True, exist_ok=True)
     cookie_setup = asyncio.run(douyin_setup(str(account_file), handle=True))
